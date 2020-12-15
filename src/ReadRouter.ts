b7633268e3a121dd97b961b7f0fbcba3b4d3a769
tonghoangvu <tonghoangvu@outlook.com>
2020-12-15 22:58:22 +0700

    Adding global config

--- src/ReadRouter.ts
+++ src/ReadRouter.ts
@@ -1,17 +1,14 @@
 import * as express from 'express';
 
-import INumberData from './INumberData';
-import IReadConfig from './IReadConfig';
-import Reader from './Reader';
+import INumberData from './reader/INumberData';
+import Config from './reader/Config';
+import Reader from './reader/Reader';
 
 const ReadRouter: express.Router = express.Router();
 
 const NUMBER_QUERY_PARAM: string = 'number';
 const SEPARATOR_QUERY_PARAM: string = 'separator';
 const UNIT_QUERY_PARAM: string = 'unit';
-
-const DEFAULT_SEPARATOR: string = ' ';
-const DEFAULT_UNIT: string = 'đơn vị';
 
 ReadRouter.get('/read', function (req: express.Request, res: express.Response) {
     // Read query params
@@ -24,28 +21,25 @@ ReadRouter.get('/read', function (req: express.Request, res: express.Response) {
         return res.end();
     const numberStr: string = paramNumber.toString();
 
-    // Optional params (has default values)
-    const separator: string = (paramSeparator)
-        ? paramSeparator.toString() : DEFAULT_SEPARATOR;
-    const unit: string = (paramUnit)
-        ? paramUnit.toString() : DEFAULT_UNIT;
+    // Modify global config
+    if (paramSeparator)
+        Config.SEPARATOR = paramSeparator.toString();
+    if (paramUnit)
+        Config.UNIT = paramUnit.toString();
 
     // Try parse to number data
     let numberData: INumberData;
     try {
         numberData = Reader.parseNumberData(numberStr);
-
-        // Build config
-        const config: IReadConfig = { separator, unit };
-
-        // Generate result
-        const result: string = Reader.readVietnameseNumber(numberData, config);
-
-        // Return JSON
-        return res.json({ text: result });
     } catch (e) {
         return res.status(400).json({ error: e.message });
     }
+
+    // Generate result
+    const result: string = Reader.readVietnameseNumber(numberData);
+
+    // Return JSON
+    return res.json({ text: result });
 });
 
 export default ReadRouter;
